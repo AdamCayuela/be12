47f621afef2455707a8933d19e048a0d6142065f
AdamCayuela <cayuelaadam@gmail.com>
2026-04-19 05:46:59 +0200

    Ajout pi4j jars dans classpath compile et run (gpiod pour RPi5)

--- run_rpi.sh
+++ run_rpi.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # ============================================================
 # Script de compilation et lancement - Simulation Tour de Piste
-# Raspberry Pi - Java 23 + JavaFX 25
+# Raspberry Pi - Java 23 + JavaFX 25 + pi4j (gpiod)
 # ============================================================
 
 JAVA=/opt/java23/bin/java
 JAVAC=/opt/java23/bin/javac
 JAVAFX=/opt/javafx-25/lib
+PI4J=/opt/pi4j/lib
 LIB=/home/cayuelad/libs/JavaFX17_Obj.jar
 SRC=~/be12/JavaApp/src
 OUT=~/be12/out
+
+# Classpath : lib perso + pi4j (core + gpiod plugin + library gpiod)
+CP=$LIB:$PI4J/pi4j-core.jar:$PI4J/pi4j-plugin-gpiod.jar:$PI4J/pi4j-library-gpiod.jar
 
 echo ">>> Compilation..."
 mkdir -p $OUT
@@ -17,7 +21,7 @@ mkdir -p $OUT
 $JAVAC \
   --module-path $JAVAFX \
   --add-modules javafx.controls,javafx.fxml,javafx.graphics \
-  -cp $LIB \
+  -cp $CP \
   -d $OUT \
   $(find $SRC -name "*.java")
 
@@ -36,5 +40,5 @@ $JAVA \
   -Dprism.forceGPU=true \
   --module-path $JAVAFX \
   --add-modules javafx.controls,javafx.fxml,javafx.graphics \
-  -cp $OUT:$LIB \
+  -cp $OUT:$CP \
   Main
